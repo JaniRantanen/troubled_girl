5f494d0f18c1cc61895e25146bac1d3690929315
janibala <35794134+JaniRantanen@users.noreply.github.com>
2019-11-24 21:24:32 +0200

    Updated main menu

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -7,19 +7,12 @@ export class MainMenu extends Phaser.Scene {
     }
 
     create() {
-
-        this.add.image(0, 0, "Taustataivas").setOrigin(0, 0).setDepth(-10)
-        this.add.image(0, 0, "taaimmaisetpuut").setOrigin(0, 0).setDepth(-8)
-        this.add.image(0, 0, "keskimmaisetpuut").setOrigin(0, 0).setDepth(-6)
-        this.add.image(0, 0, "etummaisetpuut").setOrigin(0, 0).setDepth(-4)
-
-        this.cameras.main.fadeIn(500);
-
         let { width, height } = this.sys.game.canvas;
-        let gameTitleText = this.add.existing(new TextElement(this, (width / 2), (height / 3), "TROUBLED GIRL", { font: "128px Cinzel Decorative" }).setShadow(5, 5, "#000"))
+        this.add.image(0, 0, "kansikuva_final").setOrigin(0, 0).setDepth(-2);
+        this.cameras.main.fadeIn(1000);
 
-        let startGameButton = this.add.existing(new InteractiveButton(this, (width / 2), (height / 3) + 200, "Start", { font: "64px Montserrat" }, "button"));
-        startGameButton.onClick = () => this.scene.start("sandbox");
+        let startGameButton = this.add.existing(new InteractiveButton(this, (width / 2), 600, "Start", { font: "64px Montserrat" }, "button"));
+        startGameButton.onClick = () => this.scene.start("home");
 
         let versionData = this.add.existing(new TextElement(this, (width / 2), (height / 3) + 500, `V. ${version}`, { font: "20px Montserrat" }));
 
