8564d1fcdef536df4cda95f4b123d0188f273294
janibala <35794134+JaniRantanen@users.noreply.github.com>
2019-11-24 21:21:11 +0200

    Initial ui scene

---
+++ src/scenes/utility/Dialog.js
@@ -0,0 +1,25 @@
+import { TextElement } from "../../UI";
+import { Pause } from "../../utils/utils";
+
+
+
+export class Dialog extends Phaser.Scene {
+
+	constructor() {
+		super({ key: 'dialog', active: true });
+		this.dialogText = null;
+	}
+	create() {
+		let xPosition = this.sys.canvas.width / 2;
+		let yPosition = this.sys.canvas.height - 50;
+		this.dialogText = new TextElement(this, xPosition, yPosition, '', { fontSize: "80px", fontFamily: "Montserrat", fill: '#fff', align: "center" });
+		this.add.existing(this.dialogText);
+	}
+
+	async updateDialog(text, timeToLive = 2000) {
+		this.dialogText.setText(text);
+		this.dialogText.setVisible(true);
+		await Pause(timeToLive);
+		this.dialogText.setVisible(false);
+	}
+}
