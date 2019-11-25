2eb84a19c72ea6f7f5742308e6a5bedde3e08b22
janibala <35794134+JaniRantanen@users.noreply.github.com>
2019-11-25 20:14:15 +0200

    Dont know, don't care. These will be removed anyways

--- src/scenes/testLevels/Battleground.js
+++ src/scenes/testLevels/Battleground.js
@@ -1,44 +1,27 @@
 import { SimpleEnemy } from "../../characters/SimpleEnemy";
 import { Player } from "../../characters/Player";
 import { Exit } from "../../items/Exit";
+import { setupLevel } from "../../utils/utils";
+import { ShadowEnemy } from "../../characters/ShadowEnemy";
 
 export class Battleground extends Phaser.Scene {
 	constructor() {
 		super({ key: "battleground" });
-
-		this.controls = null;
 		this.player = null;
-		this.debugText = null;
-
 	}
-
-	preload() {
-
-	}
-
 	create() {
-		//WORLD
-		let worldWidth = 1600 * 3;
-		this.impact.world.setBounds(0, 0, worldWidth, 1800);
-
-		// LEVEL
-		var map = this.make.tilemap({ key: "battleground" });
-		var tileset = map.addTilesetImage("../assets/tilesets/tileset.png", "tiles");
-		var layer = map.createStaticLayer("map", tileset, 0, 0);
-		this.impact.world.setCollisionMap("battleground");
-
-		// PLAYER
 		this.player = new Player(this, 225, 1600);
-
-		// CAMERAS
-		this.cameras.main.setBounds(0, 0, worldWidth, 1800);
 		this.cameras.main.startFollow(this.player.sprite, true, 0.1, 0.1);
+		this.cameras.main.setZoom(0.5)
+		setupLevel(this, "battleground");
 
 		this.enemies = [
 			new SimpleEnemy(this, 800, 1600),
 			new SimpleEnemy(this, 1400, 1600),
-			new SimpleEnemy(this, 1800, 1600)
+			new SimpleEnemy(this, 1800, 1600),
+			new ShadowEnemy(this, 1600, 1000)
 		];
+
 		this.exits = [new Exit(this, {
 			x: 4700,
 			y: 1200,
@@ -54,8 +37,4 @@ export class Battleground extends Phaser.Scene {
 			}
 		})]
 	}
-
-	update(time, delta) {
-	}
-
 }
