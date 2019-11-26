5fcb53db50df502b0c7c844402cdf4ae4d83ca64
janibala <35794134+JaniRantanen@users.noreply.github.com>
2019-11-26 18:25:14 +0200

    Added menu music according to spec

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -7,15 +7,26 @@ export class MainMenu extends Phaser.Scene {
     }
 
     create() {
-        let { width, height } = this.sys.game.canvas;
-        this.add.image(0, 0, "kansikuva_final").setOrigin(0, 0).setDepth(-2);
+        let introMusic = this.sound.add("alkuvalikkopiisi_1");
+        introMusic.play()
+        introMusic.once("complete", () => this.sound.play("alkuvalikkopiisi_2", { loop: true }), this);
+
+        this.createMenu();
+
         this.cameras.main.fadeIn(1000);
+    }
 
+    createMenu() {
+        let backgroundImage = this.add.image(0, 0, "kansikuva_final").setOrigin(0, 0).setDepth(-2);
+        let { width, height } = this.sys.game.canvas;
         let startGameButton = this.add.existing(new InteractiveButton(this, (width / 2), 600, "Start", { font: "64px Montserrat" }, "button"));
-        startGameButton.onClick = () => this.scene.start("home");
-
+        startGameButton.onClick = this.startGame.bind(this);
         let versionData = this.add.existing(new TextElement(this, (width / 2), (height / 3) + 500, `V. ${version}`, { font: "20px Montserrat" }));
+    }
 
+    startGame() {
+        this.sound.stopAll();
+        this.scene.start("home");
     }
 
 }
