dbb1df323f1aebe679dfe817da1cd1d25a1cb7ee
janibala <35794134+JaniRantanen@users.noreply.github.com>
2019-11-28 09:39:21 +0200

    Savepoint commit

--- src/scenes/Sandbox.js
+++ src/scenes/Sandbox.js
@@ -1,33 +1,93 @@
 
 
 import { Player } from "../characters/Player";
-import { setupLevel } from "../utils/utils";
+import { setupLevel, Pause } from "../utils/utils";
 import { SimpleEnemy } from "../characters/SimpleEnemy";
+import { ShadowEnemy } from "../characters/ShadowEnemy";
 import { DraggableItem } from "../items/DraggableItem";
+import { Toy } from "../items/Toy";
+import { Checkpoint } from "../items/Checkpoint";
 
 export class Sandbox extends Phaser.Scene {
     constructor() {
         super({ key: "sandbox" });
         this.player = null;
-        this.sounds = null;
+        this.dialogScene = null;
+        this.musicScene = null;
     }
-    create() {
+
+    preload() {
+        this.dialogScene = this.scene.get("dialog");
+        this.musicScene = this.scene.get("music");
+        this.musicScene.backgroundMusic = this.sound.add("metsamusiikki", { loop: true, volume: 0.2 });
+    }
+
+    async create() {
         this.cameras.main.setBackgroundColor(0xb9b9b9);
-        this.player = new Player(this, 200, 1600);
-        this.registry.set("dragUnlocked", true);
+        let floor_1_y = 1300;
+        this.player = new Player(this, 6500, floor_1_y);
         let level = setupLevel(this, "sandbox");
         this.cameras.main.startFollow(this.player.sprite, true, 0.1, 0.1);
-        this.cameras.main.setZoom(0.5);
 
-        let enemy_1 = new SimpleEnemy(this, 1400, 1600);
-        let box = new DraggableItem(this, 1200, 500);
+        let bear = new Toy(this, 350, floor_1_y, "item_nalle", this.bearPickup.bind(this));
+        let box = new DraggableItem(this, 600, floor_1_y, "koti_laatikko");
+        let box2 = new DraggableItem(this, 1000, floor_1_y, "koti_laatikko");
 
+        let balloon = new Toy(this, 3000, floor_1_y, "item_ilmapallo", this.balloonPickup.bind(this));
 
+        let ball = new Toy(this, 5000, floor_1_y, "item_pallo", this.ballPickup.bind(this));
+
+        let checkpoint = new Checkpoint(this, 6500, floor_1_y)
+        let basicEnemy = new SimpleEnemy(this, 7400, floor_1_y);
+        let basicEnemy_2 = new SimpleEnemy(this, 7700, floor_1_y);
+
+        let chaser = new ShadowEnemy(this, 9000, floor_1_y);
     }
 
     update() {
-
+        this.musicScene.handleMusicUpdate(this);
     }
 
+    async bearPickup() {
+        this.registry.set("dragUnlocked", true);
+
+        this.startCutscene();
+        await Pause(1000);
+        await this.dialogScene.updateDialog("Mr. Cuddles has given you the strength to push and pull things.", 2000);
+        await Pause(1000);
+        this.endCutscene();
+    }
+
+    async ballPickup() {
+        this.registry.set("dashUnlocked", true);
+        this.registry.set("slideUnlocked", true);
+
+        this.startCutscene();
+        await Pause(1000);
+        await this.dialogScene.updateDialog("You have unlocked the joy of leaping and sliding", 2000);
+        await Pause(1000);
+        this.endCutscene();
+    }
+
+    async balloonPickup() {
+        this.registry.set("doublejumpUnlocked", true);
+
+        this.startCutscene()
+        await Pause(1000);
+        await this.dialogScene.updateDialog("You have remembered the magic of walking in the air", 2000);
+        await Pause(1000);
+        this.endCutscene();
+    }
+
+    startCutscene() {
+        this.input.keyboard.enabled = false;
+        this.input.keyboard.resetKeys();
+        this.cameras.main.fadeOut(500);
+    }
+
+    endCutscene() {
+        this.input.keyboard.enabled = true;
+        this.cameras.main.fadeIn(500);
+    }
 
 }
--- src/scenes/utility/_DEBUG.js
+++ src/scenes/utility/_DEBUG.js
@@ -1,26 +1,36 @@
 export class _DEBUG extends Phaser.Scene {
 
 	constructor() {
-		super({ key: '_DEBUG', active: false });
+		super({ key: '_DEBUG', active: true });
 	}
 
 	create() {
 		this.fps = this.add.text(0, 0, '', { font: '12px Arial', fill: '#fff' });
 		this.debugTextLeft = this.add.text(0, 50, '', { font: '12px Arial', fill: '#fff' });
 		this.debugTextCenter = this.add.text(700, 0, '', { font: '12px Arial', fill: '#fff' });
+		this.debugTextBottomCenter = this.add.text(700, 650, '', { font: '12px Arial', fill: '#fff' });
 		this.debugTextRight = this.add.text(1300, 0, '', { font: '12px Arial', fill: '#fff' });
 
-		let sceneBeingDebugged = this.scene.get('home');
+		let sceneBeingDebugged = this.scene.get('sandbox');
 
 		sceneBeingDebugged.events.on('update', function () {
 			this.fps.setText(`FPS: ${sceneBeingDebugged.sys.game.loop.actualFps}`);
 			this.debugTextLeft.setText(JSON.stringify(sceneBeingDebugged.player.sprite.body, null, 4));
 			this.debugTextRight.setText(JSON.stringify(sceneBeingDebugged.player.sprite, null, 4));
-
+			this.debugTextCenter.setText(JSON.stringify(sceneBeingDebugged.player.state, null, 4));
 		}, this);
 
 		sceneBeingDebugged.events.on('debug', function (dataObject) {
-			this.debugTextCenter.setText(JSON.stringify(dataObject, null, 4));
-		}, this)
+			this.debugTextBottomCenter.setText(JSON.stringify(dataObject, null, 4));
+		}, this);
+
+		let skillUnlock = this.input.keyboard.createCombo("GIRL", { resetOnMatch: true, maxKeyDelay: 1000 });
+		this.input.keyboard.on('keycombomatch', function (keyCombo, keyboardEvent) {
+			this.scene.get("dialog").updateDialog("CHEAT MODE ACTIVATED - ALL ABILITIES UNLOCKED", 2000);
+			this.registry.set("dragUnlocked", true);
+			this.registry.set("dashUnlocked", true);
+			this.registry.set("slideUnlocked", true);
+			this.registry.set("doublejumpUnlocked", true);
+		}, this);
 	}
 }
