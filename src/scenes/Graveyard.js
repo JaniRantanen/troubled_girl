facf41be4a5f67152b64d29023f135815fadb034
janibala <35794134+JaniRantanen@users.noreply.github.com>
2019-11-28 13:10:59 +0200

    initial graveyard

--- src/scenes/Graveyard.js
+++ src/scenes/Graveyard.js
@@ -15,6 +15,15 @@ export class Graveyard extends Phaser.Scene {
 		this.cameras.main.setBackgroundColor(0xb9b9b9);
 		this.player = new Player(this, 300, 1000);
 		this.cameras.main.startFollow(this.player.sprite, true, 0.1, 0.1);
-		setupLevel(this, "graveyard");
+		let level = setupLevel(this, "graveyard");
+		this.cameras.main.setZoom(0.2);
+
+		for (let i = 0; i < level.widthInPixels; i = i + 1600) {
+			this.add.image(i, -100, "tausta_hautausmaa").setOrigin(0, 0).setDepth(-10);
+		}
+		this.impact.add.sprite(6000, 1500, "aiti_muutos7fps", 0);
+		this.impact.add.sprite(6500, 1500, "item_hauta");
+
+
 	}
 }
