8a3679a931f07843853b9207da3749f713c3e6e3
janibala <35794134+JaniRantanen@users.noreply.github.com>
2019-11-29 09:20:15 +0200

    Added scene bootstrapping function for easier level management and a "nothingIsPressed" utility function to get if no defined player control keys are pressed at current time

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import { Player } from "../characters/Player";
+
 export const Pause = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
 export function setupLevel(scene, levelKey) {
@@ -6,7 +8,6 @@ export function setupLevel(scene, levelKey) {
 	levelMap.tilesets.forEach((tileset) => {
 		let tilesetkey = tileset.name.split("/").pop().split(".").shift(); // Grabs the filename without the extension
 		let tilesetImage = levelMap.addTilesetImage(tileset.name, tilesetkey, 100, 100, 1, 2);
-
 		if (tilesetkey.search("foreground") !== -1) {
 			levelMap.createStaticLayer("foreground", tilesetImage, 0, 0).setDepth(2);
 		}
@@ -22,6 +23,18 @@ export function setupLevel(scene, levelKey) {
 	return levelMap;
 }
 
+export function setupScene(scene, level, backgroundKey, startPosition) {
+	scene.dialogScene = scene.scene.get("dialog");
+	scene.musicScene = scene.scene.get("music");
+
+	for (let i = 0; i < level.widthInPixels; i = i + 1600) {
+		scene.add.image(i, -100, backgroundKey).setOrigin(0, 0).setDepth(-10);
+	}
+
+	scene.player = new Player(scene, startPosition.x, startPosition.y);
+	scene.cameras.main.startFollow(scene.player.sprite, true, 0.1, 0.1);
+}
+
 export function disableControls(scene) {
 	scene.input.keyboard.enabled = false;
 	scene.input.keyboard.resetKeys();
@@ -30,3 +43,14 @@ export function disableControls(scene) {
 export function enableControls(scene) {
 	scene.input.keyboard.enabled = true;
 }
+
+
+export function nothingIsPressed(controlsObject) {
+	for (let [key, value] of Object.entries(controlsObject)) {
+		if (value.isDown) {
+			return false;
+		}
+	}
+	return true;
+}
+
