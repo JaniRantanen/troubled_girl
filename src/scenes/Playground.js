a897755667e717fcd0d8877c4888059bf0ce7013
Honk1n <harri.honk1@gmail.com>
2019-11-30 00:02:01 +0200

    leikkikentta paivitys

--- src/scenes/Playground.js
+++ src/scenes/Playground.js
@@ -17,26 +17,30 @@ export class Playground extends Phaser.Scene {
 	async create() {
 		let groundLevel = 1600;
 		this.level = setupLevel(this, "playground");
-		setupScene(this, this.level, "tausta_leikkikentta", { x: 150, y: groundLevel })
-
+		setupScene(this, this.level, "tausta_leikkikentta", { x: 22000, y: groundLevel })
 		let objects = [
 			new SimpleEnemy(this, 3000, groundLevel),
+			new Hideout(this, 2600, groundLevel, "metsa_pensas"),
 			new Hideout(this, 4000, groundLevel, "metsa_pensas"),
 
 			new Checkpoint(this, 6000, groundLevel),
 			new SimpleEnemy(this, 8000, groundLevel),
 			new Hideout(this, 8000, groundLevel, "metsa_pensas"),
-			new DraggableItem(this, 9000, groundLevel, "leikkikentta_roskis"),
-			new SimpleEnemy(this, 11000, groundLevel),
+			new Checkpoint(this, 6000, groundLevel),
+			new SimpleEnemy(this, 10500, groundLevel),
 
 			new Checkpoint(this, 12500, 1300),
-			new SimpleEnemy(this, 13000, groundLevel),
+			new SimpleEnemy(this, 13500, groundLevel),
 			new DraggableItem(this, 15500, groundLevel, "leikkikentta_roskis"),
 
-			new Checkpoint(this, 16000, groundLevel),
+		
 			new Toy(this, 18700, groundLevel, "item_pallo", dashSlideUnlock.bind(this, this)),
+			new Checkpoint(this, 22600, groundLevel, 150, 1700),
+			new Hideout(this, 24450, groundLevel, "metsa_pensas"),
 			new SimpleEnemy(this, 25000, groundLevel),
-			new SimpleEnemy(this, 28000, groundLevel)
+			new Hideout(this, 26200, groundLevel, "metsa_pensas"),
+			new Hideout(this, 27400, groundLevel, "metsa_pensas"),
+			new SimpleEnemy(this, 27100, groundLevel)
 		];
 
 		let exitTrigger = new Trigger(this, this.level.widthInPixels - 300, 0, 300, this.level.heightInPixels, () => {
